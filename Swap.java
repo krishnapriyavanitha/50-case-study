067a2e21206552dc5a1cffaf38e74aef36924e91
krishnapriyavanitha <krishnapriyavanitha@gmail.com>
2026-02-05 10:49:03 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,11 @@
+public class Swap{
+public static void main (String[]args){
+    int x=  20;
+    int y=- 10;
+    x= x+y;
+    y= x-y;
+    x= y-x;
+System.out.println(x);
+System.out.println(y);
+}
+}

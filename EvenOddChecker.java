132e0ac81bbba70e209587e7cc9d67ff64b037c7
krishnapriyavanitha <krishnapriyavanitha@gmail.com>
2026-02-05 11:11:30 +0530

    Create EvenOddChecker.java

---
+++ EvenOddChecker.java
@@ -0,0 +1,8 @@
+    import java.util.Scanner;
+class EvenOddChecker{
+public static void main(String[] a){
+Scanner s=new Scanner(System.in); int n=s.nextInt();
+System.out.println(n%2==0 ? "Even" : "Odd");
+}
+}
+

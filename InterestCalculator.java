c9b3bf21f2a137e79c692d943541d104aa7b5db1
krishnapriyavanitha <krishnapriyavanitha@gmail.com>
2026-02-09 17:48:53 +0530

    Add InterestCalculator class with simple interest method

---
+++ InterestCalculator.java
@@ -0,0 +1,6 @@
+class InterestCalculator{
+double si(double p,double r,double t){return (p*r*t)/100;}
+public static void main(String[] a){
+InterestCalculator o=new InterestCalculator();
+System.out.println(o.si(1000,5,2));
+}}

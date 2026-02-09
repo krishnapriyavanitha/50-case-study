fd57ed0e0fa88e39566f777b0039f9baf7d52ca6
krishnapriyavanitha <krishnapriyavanitha@gmail.com>
2026-02-09 13:47:58 +0530

    Add CharacterEncodingTool to convert char to int

---
+++ CharacterEncodingTool.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class CharacterEncodingTool{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+char c=s.next().charAt(0);
+System.out.println((int)c);
+}}

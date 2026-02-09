f0585c4c9a0277e37664d74024b2169544733040
krishnapriyavanitha <krishnapriyavanitha@gmail.com>
2026-02-09 13:49:16 +0530

    Implement basic temperature conversion from Celsius to Fahrenheit

---
+++ TemperatureMonitoringSystem.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class TemperatureMonitoringSystem{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+double c=s.nextDouble();
+System.out.println((c*9/5)+32);
+}}

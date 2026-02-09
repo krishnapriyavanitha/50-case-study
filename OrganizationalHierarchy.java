b147f2a10c5d69f300dc5686d85bfd5cb1054683
krishnapriyavanitha <krishnapriyavanitha@gmail.com>
2026-02-09 17:54:39 +0530

    Create OrganizationalHierarchy.java

---
+++ OrganizationalHierarchy.java
@@ -0,0 +1,4 @@
+class Employee{void role(){System.out.println("Employee");}}
+class Manager extends Employee{public static void main(String[] a){
+Manager m=new Manager(); m.role(); System.out.println("Manager");
+}}
